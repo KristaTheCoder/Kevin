9897208f1adef680998b86b177b6cc93f611654f
Krista <kshuckerow@gmail.com>
2017-11-07 18:22:21 -0500

    Started a player class to model a player in the game

--- src/game_rules.py
+++ src/game_rules.py
@@ -3,3 +3,9 @@ Game Rules for 3-player Kuhn Poker
 '''
 
 deck = dict([('jack', 0), ('queen', 1), ('king', 2), ('ace', 3)])
+
+#There are Three players
+#single round of betting
+#mandatory bet at the beginning
+# we need to build three player objects
+#need to play rounds
