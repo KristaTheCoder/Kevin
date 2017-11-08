5decc83d690d803aaeb0127ce1430e0598f0e204
Krista <kshuckerow@gmail.com>
2017-11-08 16:53:37 -0500

    finished up the player class for basic set up

--- src/player.py
+++ src/player.py
@@ -1,16 +1,39 @@
 '''
-Class for players in
+Class for players who can play the game, and actions they can take in the game
 '''
 
 class Player(object):
     """Build a new player"""
     def __init__(self, card):
-        superPlayer, self).__init__()
-        self.card = card
+        superPlayer, self.__init__()
+        self.card = card #expand this so that the same player can play multiple times
         self.money = 1000000
+        self.inGame = True
 
-    def bet(val):
-        self.money = self.money - val
+    def antes(chip):
+        #Enter the game by betting a first chip
+        self.money = self.money - chip
+
+    def bet(chip):
+        self.money = self.money - chip
 
     def check():
         bet(0)
+
+    def fold():
+        self.inGame = False
+
+    def set_card(card):
+        ''' This method is for when the same player participates in multiple games'''
+        self.card = card
+
+    #add all the getters
+
+    def show_hand():
+        return self.card
+
+    def is_playing():
+        return self.inGame
+
+    def get_money():
+        return self.money
