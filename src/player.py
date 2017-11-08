f219dab1e90b393756d22ace4deaff7930ea06ca
Krista <kshuckerow@gmail.com>
2017-11-08 17:08:00 -0500

    Fixed methods missing the self parameter

--- src/player.py
+++ src/player.py
@@ -2,38 +2,44 @@
 Class for players who can play the game, and actions they can take in the game
 '''
 
-class Player(object):
+class Player:
     """Build a new player"""
     def __init__(self, card):
-        superPlayer, self.__init__()
         self.card = card #expand this so that the same player can play multiple times
         self.money = 1000000
         self.inGame = True
 
-    def antes(chip):
+    def antes(self, chip):
         #Enter the game by betting a first chip
         self.money = self.money - chip
 
-    def bet(chip):
+    def bet(self, chip):
         self.money = self.money - chip
 
-    def check():
+    def check(self):
         bet(0)
 
-    def fold():
+    def fold(self):
         self.inGame = False
 
-    def set_card(card):
+    def set_card(self, card):
         ''' This method is for when the same player participates in multiple games'''
         self.card = card
 
+    def new_round(self):
+        self.inGame = True
+
     #add all the getters
 
-    def show_hand():
+    def show_hand(self):
         return self.card
 
-    def is_playing():
+    def is_playing(self):
         return self.inGame
 
-    def get_money():
+    def get_money(self):
         return self.money
+
+
+you = Player( "ace")
+print you.is_playing()
