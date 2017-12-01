0d97cb40cd95568fb9d3d0cb865ba6b633117d64
Dale Keith <dkeit008@fiu.edu>
2017-11-30 19:30:53 -0500

    Beginning

--- src/kuhn3p/players/Kevin.py
+++ src/kuhn3p/players/Kevin.py
@@ -6,6 +6,20 @@ class Kevin(Player):
 		pass
 
 	def act(self, state, card):
+		if betting.can_bet(state):
+    		if card == deck.ACE:
+				return betting.BET
+			else:
+				return betting.CHECK
+			else:
+				return betting.BET
+		else:
+			if card == deck.ACE:
+				return betting.CALL
+			else:
+				return betting.FOLD
+    			
+		
 		return betting.FOLD
 
 	def __str__(self):
