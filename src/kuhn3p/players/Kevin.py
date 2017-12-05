290df80fcbeb6c7470b3d8979210faa0c4b73194
Krista <kshuckerow@gmail.com>
2017-12-05 14:40:55 -0500

    rubber duck

--- src/kuhn3p/players/Kevin.py
+++ src/kuhn3p/players/Kevin.py
@@ -3,28 +3,24 @@ from kuhn3p import betting, deck, Player
 
 class Kevin(Player):
 	def __init__(self):
-		counter = 0
+		self.counter = 0
 		#Nash
 		self.A = 1
 		self.dic = {}
 		for i in range(13):
 			for card in range(4):
-				self.dic[zip(i,card)] = []
+				self.dic[(i,card)] = []
 		self.B = 0
 
-		self.c1 = random.Random()*1000
-		self.c2 = random.Random()*1000
+		self.c1 = random.randint(0,999)
+		self.c2 = random.randint(self.c1, 1000)
 
-		if(self.c1 >= self.c2):
-			temp = self.c1
-			self.c1 = self.c2
-			self.c2 = temp
 		self.C = 0
 		self.lastGameInfo = []
 
 	def act(self, state, card):
-		counter = counter + 1
-		if(counter > self.c1 and counter < self.c2):
+		self.counter = self.counter + 1
+		if(self.counter > self.c1 and self.counter < self.c2):
 			self.C = 1
 		else:
 			self.C = 0
@@ -43,22 +39,19 @@ class Kevin(Player):
 		                return betting.BET
                     else:
                         return betting.CHECK
-		        else:
-                    return betting.CALL
+		        
 		else:
-			if card == deck.ACE or card == deck.KING:
-				return betting.CALL
-			else:
-				if(self.C == 1):
-                    if(random.Random() < random.Random()):
-						return betting.CALL
-				    else:
-						return betting.FOLD
+		    if card == deck.ACE or card == deck.KING:
+	    		return betting.CALL
+	    	    else:
+			if(self.C == 1):
+                            if(random.Random() < random.Random()):
+			    	return betting.CALL
 			    else:
-				    return betting.FOLD
+		    		return betting.FOLD
+			else:
+			    return betting.FOLD
 
-    def set_B(self):
-        pass
 
 	def __str__(self):
 		return 'Kevin The Rubber Duck'
